aebcbae7b2e2966b12e602f56cb4e00099131709
Maxwell Dantas <maxwelldsouza@hotmail.com>
2022-12-24 02:43:51 -0300

    Refatorado e adicionado a tela Favoritos

---
+++ lib/widget/stateful/para_ler_widget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:minhas_leituras/widget/screen/para_ler_tela.dart';
+
+class ParaLerWidget extends StatefulWidget {
+  const ParaLerWidget({super.key});
+
+  @override
+  State<ParaLerWidget> createState() => ParaLerTela();
+}
